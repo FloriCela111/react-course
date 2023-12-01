a82b5ebb375e803f6cf09b098a4d9b0080e16f56
Flori Cela <floricela999@gmail.com>
2023-12-01 12:01:07 +0100

    react css

--- code/07 Styling/01-starting-project/src/components/Header.jsx
+++ code/07 Styling/01-starting-project/src/components/Header.jsx
@@ -1,24 +1,13 @@
-import { styled } from 'styled-components'
-
 import logo from "../assets/logo.png";
-// import classes from "./Header.module.css";
-
-const StyleHeader = styled.header`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-margin-top: 2rem;
-margin-bottom: 2rem;
-`
 
 export default function Header() {
   return (
-    <StyleHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </StyleHeader>
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
+      <img src={logo} alt="A canvas" className="mb-8 w-44 h-44" />
+      <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+        ReactArt
+      </h1>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
+    </header>
   );
 }
-  
